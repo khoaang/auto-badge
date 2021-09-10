1ff660e93123a454804d3e7f3433f34721f55ffe
hungryboot <nguyenrt@hotmail.com>
2021-09-09 17:10:30 -0700

    started badge graphic, added print query, color picker for background

---
+++ test html/app.js
@@ -0,0 +1,28 @@
+bgSolidChoice = document.querySelector('#solid-color')
+bgGradientChoice = document.querySelector('#gradient-color')
+bgImageChoice = document.querySelector('#image-background')
+solidColorPicker = document.querySelector('#solid-color-picker')
+badge = document.querySelector('.badge')
+
+const changeBg =()=>{
+    if (bgSolidChoice.checked){
+        document.querySelector('#color-options').hidden = false
+        document.querySelector('#gradient-options').hidden = true
+        document.querySelector('#image-options').hidden = true
+        console.log('solid color picked')
+        badge.style.background = solidColorPicker.value
+    }
+    if (bgGradientChoice.checked){
+        document.querySelector('#color-options').hidden = true
+        document.querySelector('#gradient-options').hidden = false
+        document.querySelector('#image-options').hidden = true
+        console.log('gradient picked')
+    }
+    if (bgImageChoice.checked){
+        document.querySelector('#color-options').hidden = true
+        document.querySelector('#gradient-options').hidden = true
+        document.querySelector('#image-options').hidden = false
+        console.log('image picked')
+    }
+    
+}
