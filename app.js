65dd060eaf98de5a7606488824ab7b36feee8e24
Blake Haug <33404760+BNH440@users.noreply.github.com>
2021-09-10 14:34:17 -0700

    spreadsheet stuff

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
-bgSolidChoice = document.querySelector('#solid-color')
-bgGradientChoice = document.querySelector('#gradient-color')
-bgImageChoice = document.querySelector('#image-background')
-bgImageFile = document.querySelector('#bg-image-file')
-solidColorPicker = document.querySelector('#solid-color-picker')
-badge = document.querySelector('.badge')
+bgSolidChoice = document.querySelector("#solid-color");
+bgGradientChoice = document.querySelector("#gradient-color");
+bgImageChoice = document.querySelector("#image-background");
+bgImageFile = document.querySelector("#bg-image-file");
+solidColorPicker = document.querySelector("#solid-color-picker");
+badge = document.querySelector(".badge");
 
 function changeBgImg(event) {
-	url = URL.createObjectURL(event.target.files[0]);
-    console.log(url)
-	badge.style.background = `cover url(${url} no-repeat)`
+    url = URL.createObjectURL(event.target.files[0]);
+    console.log(url);
+    badge.style.background = `cover url(${url} no-repeat)`;
 }
-const changeBg =()=>{
-    if (bgSolidChoice.checked){
-        document.querySelector('#color-options').hidden = false
-        document.querySelector('#gradient-options').hidden = true
-        document.querySelector('#image-options').hidden = true
-        console.log('solid color picked')
-        badge.style.background = solidColorPicker.value
-    }
-    if (bgGradientChoice.checked){
-        document.querySelector('#color-options').hidden = true
-        document.querySelector('#gradient-options').hidden = false
-        document.querySelector('#image-options').hidden = true
-        console.log('gradient picked')
-    }
-    if (bgImageChoice.checked){
-        document.querySelector('#color-options').hidden = true
-        document.querySelector('#gradient-options').hidden = true
-        document.querySelector('#image-options').hidden = false
-        console.log('image option picked')
-    }
-    
+function uploadSpreadsheet() {
+    var file = document.querySelector("#spreadsheet-file").files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = () => {
+        console.log(reader.result);
+    };
 }
+const changeBg = () => {
+    if (bgSolidChoice.checked) {
+        document.querySelector("#color-options").hidden = false;
+        document.querySelector("#gradient-options").hidden = true;
+        document.querySelector("#image-options").hidden = true;
+        console.log("solid color picked");
+        badge.style.background = solidColorPicker.value;
+    }
+    if (bgGradientChoice.checked) {
+        document.querySelector("#color-options").hidden = true;
+        document.querySelector("#gradient-options").hidden = false;
+        document.querySelector("#image-options").hidden = true;
+        console.log("gradient picked");
+    }
+    if (bgImageChoice.checked) {
+        document.querySelector("#color-options").hidden = true;
+        document.querySelector("#gradient-options").hidden = true;
+        document.querySelector("#image-options").hidden = false;
+        console.log("image option picked");
+    }
+};
