89419eaaf681d5da9801c6a59f3064e41332dda8
Blake Haug <33404760+BNH440@users.noreply.github.com>
2021-09-21 17:38:37 -0700

    added electron

---
+++ electron/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 900,
+        height: 900,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+    win.loadFile("../index.html");
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") app.quit();
+});
